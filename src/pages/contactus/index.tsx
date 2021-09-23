edd5154c984b6c09a42c74e988257c2b8f69c88d
David Gaspar <davidgaspar.dev@gmail.com>
2021-09-23 12:37:10 -0300

    Contact Us page added

---
+++ src/pages/contactus/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components";
+
+export default function ContactUs(): JSX.Element {
+
+    return (
+        <div>
+            <Header/>
+            <h1>Contect Us Page</h1>
+        </div>
+    );
+}
