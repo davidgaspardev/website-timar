5a0a4a8a6089d6e83f18a9f835e9164117a75281
David Gaspar <davidgaspar.dev@gmail.com>
2021-09-23 11:55:46 -0300

    About and Tips pages added

---
+++ src/pages/about/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components";
+
+export default function AboutPage(): JSX.Element {
+
+    return (
+        <div>
+            <Header />
+            <h1>About Page</h1>
+        </div>
+    );
+}
---
+++ src/pages/tips/index.tsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components";
+
+export default function TipsPage(): JSX.Element {
+
+    return (
+        <div>
+            <Header />
+            <h1>Tips Page</h1>
+        </div>
+    );
+}
